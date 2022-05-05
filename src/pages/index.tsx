c13700cb8b53b8ebf37bb03b2cfa751f0b8f735f
shridharp <shridharpoman@gmail.com>
2022-05-04 23:26:29 -0400

    Home page changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { Typography } from '@mui/material';
+import { useUser } from '../context/AuthContext';
 
 
 export default function Home() {
+  const { user } = useUser();
+  console.log(user);
   return (
-    <Typography variant='h1'>Hello Cloud Project</Typography>
+    <Typography variant='h1'>Hello {user.getUsername()} to Cloud Project</Typography>
   );
 }
